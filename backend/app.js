12b2597de7a72715d43f9c4f5e6f397325764422
Timofey Pichugin <tpich87@gmail.com>
2023-06-27 16:16:15 +0300

    feat: добавлено логирование запросов

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,6 @@ app.get('/crash-test', () => {
 app.post('/signin', validatorLogin, login);
 app.post('/signup', validatorCreateUser, createUser);
 
-app.use(auth);
 app.use('/', routesUser);
 app.use('/', routesCard);
 
@@ -45,6 +44,7 @@ app.use(errorLogger);
 
 app.use(errors());
 app.use(handlerError);
+app.use(auth);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.listen(PORT, () => {
